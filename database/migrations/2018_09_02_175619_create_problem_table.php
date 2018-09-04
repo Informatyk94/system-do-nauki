fbc7591b0efba9c5711d0f80e09a3fffe9c2d636
Konrad <konradxd7@gmail.com>
2018-09-04 10:17:50 +0200

    clear projekt

---
+++ database/migrations/2018_09_02_175619_create_problem_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProblemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('problem_table', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('content_problem');
+            $table->string('content_solution');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('problem_table');
+    }
+}
